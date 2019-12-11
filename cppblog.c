6b5c2e8566a9bd887091509ac0c26da5f1fb6fed
Hoa Thiên Vũ <hoathienvu8x@gmail.com>
2019-12-11 15:06:02 +0700

    Sample code test parse infomation from path info

--- cppblog.c
+++ cppblog.c
@@ -1,8 +1,17 @@
 #include <iostream>
 #include <string>
 #include <stdlib.h>
+#include <regex>
+#include "util.h"
 #include "http.h"
 #include "html.h"
+
+std::regex make_regex(std::string re, bool ignorecase = false) {
+    if (ignorecase) {
+        return std::regex(re, std::regex_constants::ECMAScript | std::regex_constants::icase);
+    }
+    return std::regex(re);
+}
 
 int main(int argc, char **argv) {
     set_content_type("text/html; charset=utf-8");
@@ -15,6 +24,18 @@ int main(int argc, char **argv) {
     h1_tag("This is CPP Blog");
     p_tag("This is description CPP Blog");
     blockquote_tag("This is simple and the first idea blog on c code, using cgi + sqlite to store database");
+    const char *path_info = getenv("REQUEST_URI");
+    if (path_info != NULL) {
+        p_tag("Request URI: " + std::string(path_info));
+        std::regex rx = make_regex("^/tu-khoa/(.*)?/?$", true);
+        std::smatch res;
+        std::string path = std::string(path_info); // https://stackoverflow.com/a/30495370
+        if(std::regex_search(path, res, rx)) {
+            if (res.size() > 1) {
+                p_tag("Tag: " + decode_url(std::string(res[1])));
+            }
+        }
+    }
     body_end();
     html_end();
     return 0;
