b6a21202f282ea4d4bc66f481a8c4da367624c74
Hoa Thiên Vũ <hoathienvu8x@gmail.com>
2019-12-12 11:42:42 +0700

    Handle data store to sqlite

--- cppblog.c
+++ cppblog.c
@@ -5,6 +5,12 @@
 #include "util.h"
 #include "http.h"
 #include "html.h"
+#include "sqlite3.h"
+#include "dump.h"
+
+std::string current_path = "./";
+std::string dbFile = "cppblog.db";
+sqlite3 *db;
 
 std::regex make_regex(std::string re, bool ignorecase = false) {
     if (ignorecase) {
@@ -14,6 +20,45 @@ std::regex make_regex(std::string re, bool ignorecase = false) {
 }
 
 int main(int argc, char **argv) {
+    current_path = getexepath();
+    if ( ! is_dir( current_path + "datas" ) && ! mkdirAll( current_path + "datas" )) {
+        std::cout << "Could not create sqlite quote store data" << std::endl;
+		return 1;
+    }
+
+    int rc;
+    bool dbinstall = false;
+    dbFile = current_path + "datas" + PATH_SEPARATOR + dbFile;
+    if ( ! file_exists( dbFile ) ) {
+        dbinstall = true;
+    }
+    rc = sqlite3_open(dbFile.c_str(), &db);
+    if( rc != SQLITE_OK ){
+        std::cout << "DB Error: " << sqlite3_errmsg(db) << std::endl;
+        sqlite3_close(db);
+        return 1;
+    }
+    if (dbinstall) {
+        char *zErrMsg = 0;
+        rc = sqlite3_exec(db, "CREATE TABLE IF NOT EXISTS posts ( id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL, slug TEXT NOT NULL, excerpt TEXT NOT NULL, content TEXT NOT NULL, pubdate TEXT NOT NULL, tags TEXT NOT NULL );", NULL, 0, &zErrMsg);
+        if ( rc != SQLITE_OK ) {
+            std::cout << "SQL error create posts table: " << zErrMsg << std::endl;
+            sqlite3_close(db);
+            return 1;
+        }
+        rc = sqlite3_exec(db, "CREATE TABLE IF NOT EXISTS terms ( id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, slug TEXT NOT NULL );", NULL, 0, &zErrMsg);
+        if ( rc != SQLITE_OK ) {
+            std::cout << "SQL error create terms table: " << zErrMsg << std::endl;
+            sqlite3_close(db);
+            return 1;
+        }
+        rc = sqlite3_exec(db, "CREATE TABLE IF NOT EXISTS post_terms ( id INTEGER PRIMARY KEY AUTOINCREMENT, post_id INTEGER NOT NULL DEFAULT 0, term_id INTEGER NOT NULL DEFAULT 0 );", NULL, 0, &zErrMsg);
+        if ( rc != SQLITE_OK ) {
+            std::cout << "SQL error create post terms table: " << zErrMsg << std::endl;
+            sqlite3_close(db);
+            return 1;
+        }
+    }
     set_content_type("text/html; charset=utf-8");
     html_doctype();
     html_begin();
